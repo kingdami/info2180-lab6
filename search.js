1ad2565853b49d3ec2f8fa8c6c61da1576c72c53
kingdami <icemandelano@hotmail.com>
2016-11-11 12:01:09 -0500

    Update search.js

--- search.js
+++ search.js
@@ -1,3 +1,4 @@
+var response
 function handle () {
 	var httpRequest = new XMLHttpRequest();
 	var q;
@@ -24,4 +25,44 @@ function handle () {
   
 	httpRequest.send();	
 	
+}
+		
+function handleAll() {
+	var httpRequest = new XMLHttpRequest();
+	
+	httpRequest.onreadystatechange = function(){
+		
+		if(httpRequest.readyState === XMLHttpRequest.DONE) {
+			if(httpRequest.status === 200) {
+				var response = httpRequest.responseXML;
+				var resultTag = document.getElementById("result");
+				resultTag.innerHTML= "";
+				var space = document.querySelector('#result');
+				var definitions = response.getElementsByTagName('definition');
+				
+				var list = document.createElement('ol');
+				space.appendChild(list);
+				
+				for(var i = 0; i< definitions.length; i++) {
+					var heading = document.createElement('h2');
+					var footer = document.createElement('p');
+					var term = document.createTextNode(definitions[i].getAttribute("name"));
+					var author = document.createTextNode(definitions[i].getAttribute("author"));
+					heading.appendChild(term);
+					footer.appendChild(author);
+					
+					var definition = document.createElement('li');
+					var text = document.createTextNode(definitions[i].childNodes[0].nodeValue);
+					definition.appendChild(text);
+					list.appendChild(heading);
+					list.appendChild(definition);
+					list.appendChild(footer);
+				}
+			}else {
+				alert('Error request');
+			}
+		}
+	};
+	httpRequest.open("GET", "request.php?q=&all=true")
+	httpRequest.send();
 }
