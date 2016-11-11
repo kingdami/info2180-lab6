aa4164433d4e484f38a70c1eff2c770386e4a833
kingdami <icemandelano@hotmail.com>
2016-11-11 11:57:10 -0500

    Add files via upload

---
+++ search.js
@@ -0,0 +1,27 @@
+function handle () {
+	var httpRequest = new XMLHttpRequest();
+	var q;
+	response = document.getElementById("result");
+	response.innerHTML = "processing...";
+	q = document.getElementsByName("q")[0].value;
+	console.log(q);
+
+	
+	//var url = "request.php";
+	httpRequest.open("GET", "request.php?q=definition" + q, true);
+	
+	httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	httpRequest.onreadystatechange = function() {
+		if (httpRequest.readyState === XMLHttpRequest.DONE) {
+			if(httpRequest.status == 200) {
+			response = httpRequest.responseText;
+			console.log(response);
+			document.getElementById("result").innerHTML = response;
+			}
+		}
+  	};
+  
+	httpRequest.send();	
+	
+}
